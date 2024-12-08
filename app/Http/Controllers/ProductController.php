65f5788c7b5c715de78acaf84001f7ef93401f3c
ElifKumbetli <elifkumbetli@gmail.com>
2024-12-08 12:41:08 +0300

    laravel crud

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    
+    public function index()
+    {
+       $products = Product::all();
+       return view('index',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('create');
+    }
+
+    
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+        return redirect('product');
+    }
+
+   
+    public function show($id)
+    {
+        $product = Product::FindOrFail($id);
+        return view('show',compact('product'));
+    }
+
+  
+    public function edit($id)
+    {
+        $product = Product::FindOrFail($id);
+        return view('update',compact('product'));
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        $product=Product::FindOrFail($id);
+        $product->update($request->all());
+        return redirect('product');
+    }
+
+    
+    public function destroy($id)
+    {
+      $product=Product::FindOrFail($id);
+      $product->delete();
+      return redirect('product');
+    }
+}
